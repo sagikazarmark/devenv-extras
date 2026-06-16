e970766fabe1511e1daaf8e37e97fbd42311a010
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2026-06-16 22:13:57 +0200

    update dang
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- packages/dang.nix
+++ packages/dang.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "dang";
-  version = "4dc9b32d92383ca3578fcde16770fd4b3525b709";
+  version = "bfbce2f4302cb9b11d2809e9e6fa59b575b0de95";
 
   src = fetchFromGitHub {
     owner = "vito";
     repo = "dang";
     rev = version;
-    hash = "sha256-4PeBtxvGLWTIedL1f02/djZl/lcjq1dywPeSb/A4/DI=";
+    hash = "sha256-Z3UBuXXGaCzUKF4+jjPIAeZuLNNYIMIuWuLgiyrK2uc=";
   };
 
   vendorHash = "sha256-t/Lik2quiLorvi+BA44X9I46/XRNkWbkViDEDzn9n0M=";
